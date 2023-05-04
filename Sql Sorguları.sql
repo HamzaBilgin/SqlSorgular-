33330c206966939097fbdd13f7f7a9548234bca7
Hamza Bilgin <112509615+HamzaBilgin@users.noreply.github.com>
2023-05-04 20:23:18 +0300

    has been changed

--- Sql Sorguları.sql
+++ Sql Sorguları.sql
@@ -35,6 +35,68 @@ Select LastName from Employees
 Select p.ProductName from Products p
 Select e.LastName from Employees e
 
+-- Tabloda update iþlemi yapma - Veriyi güncellemeyi saðlar
+update Personel set
+Soyisim='Bilgin',
+Isim='Hamza',
+EmailAdres='update@gmail.com'
+where
+PersonelID=5
+
+-- Tabloda delete iþlemi - veriyi silmeyi saðlar
+delete Personel where PersonelID=2
+
+-- Tabloda verileri çekerken kolon isimlerini deðiþtirme
+select CompanyName as [Sirket Adi],City as Sehir from Customers
+
+--And kullanýmý - Veriyi iki veya daha fazla þarta göre çaðýrmak için kullanýlýr
+select * from Products where SupplierID = 1 and CategoryID=1
+
+-- Max ve Min kullanýmý. Tablodaki stunlardaki en yüksek ve en düþük deðeri bulmak için kullanýlýr
+select max(SupplierID) as [En yüksek ID] from Products
+
+--Sql view olarak kaydetme - Sorgu ile çaðrýlan tablo sorgu ile geldiðinden kayýt olmaz,
+---bunu istersek views olarak kaydedebiliriz. Bunu daha sonra üzerinde tekrar
+----oynamak için yaparýz. Tekrar ayný tabloyu sorgu ile çaðýrmak yerine kaydedilen tabloyu kullanýrýz.
+create view [Mexico Customers] as
+select ContactName from Customers where Country = 'Mexico'
+
+--Distinct kullanýmý - Distinct yapýlan sorgudaki sütunlardaki benzer öðeleri tekrar ---yazdýrmaz.Örneðin örnekteki gibi normalde birden çýk olan Brazil sorgu ile tek bir ----tane gözükür. Analiz yaparken iþe yarar
+select distinct Country from Customers
+
+-- Gruop by kullanýmý - Distinct ile ayný iþleve sahip. Sadece kümülatif yani toplama ---gibi iþlemlerde group by sorgu süresini kýsaltýp daha verimli sorgu yapar.
+Select Country,Count(*) Adet From Customers group by Country
+
+-- Constrain kullanýmý - Tablolar oluþtururken belli kýsýtlandýrmalar konulabilir bilir. Bu kýsýtlandýrmalara
+---Constraint denir. Aþaðýda kullanýlan contraintler ve açýklamalar mevcuttur.
+create table constrainOrnek
+(
+kolon1 int not null,  -- Bir sütunun null bir deðer almamasýný saðlar
+kolon2 int unique, -- Bu sütundaki bütün deðerler birbirinden farklý olmasýný saðlar
+kolon3 int primary key, -- not null ve unique in birleþimi gibidir
+kolon4 int foreign key references Customers(CustomersID),
+kolon5 int check ( kolon5<18), -- koþulu kontrol edip ona göre deðer almasýný saðlar
+kolon6 varchar default 3 -- Hiçbir koþul girilmediðinde otomatik olarak bu deðerin atanmasýný saðlar
+)
+
+-- Count kullanýmý - Bize istediðimiz sorguya ait kaç veri olduðunu gösterir
+select count(*) from Products where CategoryID=1
+
+-- AVG kullanýmý - Bize istediðimiz sorguda ortalama deðer çýkarýr. Örneðin
+--- Ürünlerinizin ortalama birim fiyatýný öðrenmek için kullanýlabillir
+select AVG(UnitPrice) from Products
+
+-- Top kullanýmý - Bize yapacýðýmýz sorguda örneðin ilk 10 veriyi getirmemizi saðlar
+select top 10 * from Products
+
+-- Sum kullanýmý - Bize örneðin stoktaki ürün adeti ve birim fiyatlarýnýn çarpýmým
+---sonucu getirebilmemizi saðlar
+select sum(UnitPrice*UnitsInStock) from Products
+
+-- Having kullanýmý - Aggregate functionlarda where kullanýlmaz. Yani toplama sayma gibi
+--- iþlemlerde kullanýlmaz. Bunun için having metodu getirilmiþtir ve aslýnda hep ---- group by ile kullanýlýr.
+Select Country,City,Count(*) Adet From Customers group by Country,City having Count(*)>1 order by Country  
+
 -- Ýki deðeri çarparak tek sütunda getirme
 Select p.ProductName,p.UnitsInStock * p.UnitPrice as Total from Products p
 
@@ -53,6 +115,7 @@ Select * from Products Where ProductName = 'Chai'
 Select * from Employees order by FirstName
 Select * from Orders order by CustomerID
 Select * from Products order by UnitPrice desc --terse çevirir
+
 ----order by dan sonra yazýlma sýrasýna göre sýralama
 Select * from Products order by UnitPrice,ProductName 
 Select * from Employees order by FirstName,Title
@@ -71,4 +134,24 @@ Select * from Products where UnitPrice between 10 and 50 order by UnitPrice --10
 
 -- In kullanýmý
 Select * from Products where CategoryID in (1,2) //1 veya 2 olanlar geliyor
+
+--------------------   JOÝN KULLANIMLARI    ---------------------
+--Join kullanýmý - iki tablonun eþit olduðu veriler baz alýnarak birleþtirmesini yapar
+Select * from Products inner join Categories on Products.CategoryID = Categories.CategoryID
+
+--- 3 tabloda ayný anda birleþtirme yapýlabilir. Aþaðýdaki Products ile Order Details tablosundaki
+---- ortak olan ProductID ile eþleþtirilmiþ sonra da Order Details ile Orders tablolarýndaki ortak 
+----- olan OrderID eþleþtirilip tek tablo haline getirilmiþtir.
+Select p.ProductName,o.OrderDate,od.Quantity * od.UnitPrice as TOTAL from Products p 
+inner join [Order Details] od on p.ProductID=od.ProductID 
+inner join Orders o on o.OrderID=od.OrderID
+
+-- Left Join Kullanýmý - Leftjoin kullanýrken unutlmamasý gereken nokta. Sað tarafa eklediðimiz --- tabloda sol taraftan eþleþmeyen bir yer varsa null deðer vermesidir.
+Select c.Address,c.Country,o.ShipRegion from Customers c left join Orders o
+on c.CustomerId = o.CustomerID
+
+-- Right join kullanýmý 
+Select c.Address,c.Country,o.ShipRegion from Orders o right join Customers c
+on c.CustomerId = o.CustomerID
+
 
---
+++ StoredProcedure.sql
@@ -0,0 +1,105 @@
+-- Belirli bir işlevi veya görevi yerine getirmek için yapılandırılmış sql kod parçacıklarıdır.
+--- Crud işlemler gibi her seferinde tekrar yazıp derlememiz gerekmediği için performans
+----açısından kazanç sağlar Aynı java metodları gibi input parametreleri alır ve sonucunda
+----- output yani çıktı verir. Derlendikten sonra tekrar derlemeye gerek kalmayıp veritabanında
+------ execution plan olarak saklanır.
+--Procedure oluşturma 
+Create Procedure OrdersByDate
+(
+	@OrderBegindate DATETIME,
+	@OrderEndDate Datetime,
+	@ShipCity Varchar(30)
+)
+as
+begin
+select OrderID,CustomerID,OrderDate,ShippedDate,ShipName,ShipAddress from Orders
+where OrderDate between @OrderBeginDate and  @OrderEndDate and ShipCity = @ShipCity
+end;
+
+--Oluşturulan Prosedürü çağırma
+Exec OrdersByDate
+@OrderBegindate = '1996-07-01',
+@OrderEndDate = '1997-07-01',
+@ShipCity = 'London';
+
+-- Yukarıdaki procedure sayesinde begindate enddate gibi şartlar değiştikçe tekrar gidip
+---yeni bir sql sorgusu yazmaya gerek kalmadı.
+
+-- Oluşturulan procedure de değişiklik yapmak için create yerine alter yazmak ve 
+---değiştirmek istediğimiz verileri değiştirmek yeterli.
+Alter Procedure OrdersByDate
+(
+	@OrderBegindate DATETIME,
+	@OrderEndDate Datetime,
+	@ShipCity Varchar(30)
+)
+as
+begin
+select OrderID,CustomerID,OrderDate,ShippedDate,ShipName,ShipAddress from Orders
+where OrderDate between @OrderBeginDate and  @OrderEndDate and ShipCity = @ShipCity
+end;
+
+--Oluşturulan procedure u silmek için drop yazma yeterli.
+drop Procedure OrdersByDate
+
+
+-- Procedure kilitlemek için şifreleme işlemine başvurular. Burdaki amaç procedurun 
+---değiştirilmemesini sağlar. Dikkat edilmelidir ki bizde değiştiremeyiz bu yüzden 
+--- kriptolamadan yani şifrelemeden önce bir yedeğinin alınması önemlidir.
+Create Procedure OrdersByDate
+(
+	@OrderBegindate DATETIME,
+	@OrderEndDate Datetime,
+	@ShipCity Varchar(30)
+)
+with encryption
+as
+begin
+select OrderID,CustomerID,OrderDate,ShippedDate,ShipName,ShipAddress from Orders
+where OrderDate between @OrderBeginDate and  @OrderEndDate and ShipCity = @ShipCity
+end;
+
+-- Hadi başka örneklerle pekiştirelim 
+create table Musteri
+(
+ID int primary key,
+Isim nvarchar(30),
+SoyIsim nvarchar(30),
+OlusturmaTarih datetime default getdate()
+)
+create table MusteriGirisBilgileri
+(
+MID int primary key,
+KullaniciAdi nvarchar(30) unique not null,
+Sifre nvarchar(30) check(Len(sifre)>6), -- Şifrenin kontrolü yapıldı
+foreign key(MID) references Musteri(ID) -- Burda müsteri tablosu ile bağladık.
+)
+
+--Yukaridaki iki tabloyu oluşturalım
+-- Bir tane insert işlemi içeren procedure yazalım
+create proc SP_Musteri_YeniKayit
+(
+@ID int,
+@Isim nvarchar(30),
+@Soyisim nvarchar(30),
+@KullaniciAdi nvarchar(30),
+@Sifre nvarchar(10),
+)
+as
+begin
+insert into Musteri(ID,Isim,SoyIsim) values (@ID,@Isim,@Soyisim)
+	if(@@ROWCOUNT>0)
+	begin
+		insert into MusteriGirisBilgileri(MID,KullaniciAdi,Sifre) values (@ID,@KullaniciAdi,@Sifre)
+	end
+end
+
+
+
+
+
+
+
+
+
+
